446980fcb4aee5b4f2a75b70089b8cc5b71a3bda
Klarissa Vasallo <kjvas@LAPTOP-2MT2RVTF.localdomain>
2026-05-02 00:28:01 +0800

    Feat(Seeder): Added seeder for Status

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\StatusSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        $this->call([
+            StatusSeeder::class,
+        ]);
+
         // User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
     }
 }
