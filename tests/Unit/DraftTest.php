0bb1177c41be422af415e588072e7cdcc00f8509
Klarissa Vasallo <kjvas@LAPTOP-2MT2RVTF.localdomain>
2026-05-02 01:00:54 +0800

    Feat(DraftTest): Tested the null file, the file type

---
+++ tests/Unit/DraftTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Validator;
+
+class DraftTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+
+    public function test_file_is_required_false(): void{
+
+        $validator = Validator::make(
+            ['file' =>null],
+            ['file' => 'required|file|mimes:pdf|max:10240']
+        );
+
+        $this->assertFalse($validator->passes());
+    }
+
+    public function test_file_is_required_true(): void{
+          $file = UploadedFile::fake()->create('document.pdf',100);
+        $validator = Validator::make(
+            ['file' => $file],
+            ['file' => 'required|file|mimes:pdf|max:10240']
+        );
+
+        $this->assertTrue($validator->passes());
+    }
+
+
+    public function test_non_pdf_file_is_invalid(): void
+    {
+        $file = UploadedFile::fake()->create('image.png',100);
+
+        $validator = Validator::make(
+            ['file' => $file],
+            ['file' => 'required|file|mimes:pdf|max:10240']
+        );
+
+        $this->assertFalse($validator->passes());
+    }
+
+    public function test_pdf_file_is_valid(): void
+    {
+        $file = UploadedFile::fake()->create('document.pdf', 100);
+
+        $validator = Validator::make(
+            ['file' => $file],
+            ['file' => 'required|file|mimes:pdf|max:10240'],
+        );
+
+        $this->assertTrue($validator->passes());
+    }
+}
