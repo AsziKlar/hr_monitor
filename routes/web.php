5a295bc793b5228f559191afd36d5e07df5d0cbd
Klarissa Vasallo <kjvas@LAPTOP-2MT2RVTF.localdomain>
2026-05-04 02:41:05 +0800

    Fix(web.php): removed a redundant line.

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\DraftController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\DraftController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
