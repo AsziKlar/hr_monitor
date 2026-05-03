d2f7ad258236061143df13429c71a57fcb979d87
AsziKlar <20220026715@my.xu.edu.ph>
2026-05-04 02:59:35 +0800

    Fix(Routes): Add names to drafts create() and store() routes

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::get('/drafts', [DraftController::class, 'index'])->name('drafts.index');
-Route::get('/drafts/create', [DraftController::class, 'create']);
-Route::post('/drafts', [DraftController::class, 'store']);
+Route::get('/drafts/create/{mechanism}', [DraftController::class, 'create'])->name('drafts.create');
+Route::post('/drafts', [DraftController::class, 'store'])->name('drafts.store');
 
 Route::get('/drafts/{id}', [DraftController::class, 'show'])->name('drafts.show');
 
